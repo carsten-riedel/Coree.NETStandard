aac7c1d59efb727b87c17b21dd6ea366c1a25725
Carsten Riedel <carsten.riedel.one@outlook.com>
2024-03-01 17:07:56 +0100

    update before removal

---
+++ src/Projects/Coree.NETStandard/Extensions/ServiceCollection.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using Coree.NETStandard.Options;
+
+namespace Coree.NETStandard.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHostedServices<T>(this IServiceCollection services) where T : class, IHostedService
+        {
+            return services.AddSingleton<IHostedService, T>();
+        }
+
+        public static IServiceCollection AddHostedServicesWithOptions<T, K>(this IServiceCollection services, K[] values) where K : class, new() where T : class, IHostedService
+        {
+            var optionsProvider = new HostedServicesWithOptionsProvider<K>();
+            foreach (var value in values)
+            {
+                optionsProvider.Enqueue(value);
+            }
+            services.AddSingleton<IHostedServicesWithOptionsProvider<K>>(optionsProvider);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                services.AddSingleton<IHostedService, T>();
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddHostedServicesWithOptions<T, K>(this IServiceCollection services, IConfiguration configuration, string sectionName) where K : class, new() where T : class, IHostedService
+        {
+            var optionsProvider = new HostedServicesWithOptionsProvider<K>();
+            services.AddSingleton<IHostedServicesWithOptionsProvider<K>>(optionsProvider);
+
+            var configSection = configuration.GetSection(sectionName);
+            foreach (var childSection in configSection.GetChildren())
+            {
+                var configInstance = new K();
+                childSection.Bind(configInstance);
+                optionsProvider.Enqueue(configInstance);
+            }
+
+            for (int i = 0; i < configSection.GetChildren().Count(); i++)
+            {
+                services.AddSingleton<IHostedService, T>();
+            }
+
+            return services;
+        }
+    }
+}
---
+++ src/Projects/Coree.NETStandard/Options/HostedServicesWithOptionsProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coree.NETStandard.Options
+{
+    public interface IHostedServicesWithOptionsProvider<T>
+    {
+        void Enqueue(T item);
+
+        T NextOption();
+    }
+
+    public class HostedServicesWithOptionsProvider<T> : IHostedServicesWithOptionsProvider<T> where T : new()
+    {
+        private ConcurrentQueue<T> OptionsQueue { get; set; } = new ConcurrentQueue<T>();
+
+        public void Enqueue(T item)
+        {
+            OptionsQueue.Enqueue(item);
+        }
+
+        public T NextOption()
+        {
+            var result = OptionsQueue.TryDequeue(out T? resultItem);
+            if (!result)
+            {
+                return new T();
+            }
+            return resultItem;
+        }
+    }
+}
