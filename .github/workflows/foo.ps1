898d3cb53bc8aaf3d43e0e85f4bbb128ddf1ba21
Carsten Riedel <carsten.riedel.one@outlook.com>
2024-03-16 20:37:09 +0100

    update foo

--- .github/workflows/foo.ps1
+++ .github/workflows/foo.ps1
@@ -3,12 +3,21 @@ param (
     [string]$server
 )
 
-. "$PSScriptRoot/secrets.ps1"
+$gitroot = git rev-parse --show-toplevel 2>&1
+
+$secretsPath = "$gitroot/.github/workflows/secrets.ps1"
+
+# Check if the secrets file exists before importing
+if (Test-Path $secretsPath) {
+    . $secretsPath
+    Write-Output "Imported secrets from: $secretsPath"
+} else {
+    Write-Output "Secrets file not found at: $secretsPath"
+}
 
 # Fetch the current git branch name
 $gitBranch = git rev-parse --abbrev-ref HEAD
 
-$gittld = git rev-parse --show-toplevel 2>&1
-
-# Output the server parameter and the git branch name
-Write-Output "Server: $server, Git Branch: $gitBranch , $FOO $gittld"
+# Output the server parameter, the git branch name, and if available, the $FOO variable from secrets.ps1
+$fooOutput = if ($null -ne $FOO) { $FOO } else { "FOO not set" }
+Write-Output "Server: $server, Git Branch: $gitBranch, FOO: $fooOutput, Git root: $gitroot"
