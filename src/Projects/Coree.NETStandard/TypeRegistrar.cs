ff8e76b26861d06cf9786896711376ef69dd6941
Updated form Workflow <carstenriedel@outlook.com>
2024-03-22 08:34:07 +0100

    update

---
+++ src/Projects/Coree.NETStandard/TypeRegistrar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Spectre.Console.Cli;
+
+namespace Coree.NETStandard
+{
+    public sealed class TypeRegistrar : ITypeRegistrar
+    {
+        private readonly IServiceCollection services;
+
+        public TypeRegistrar(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        public ITypeResolver Build()
+        {
+            return new TypeResolver(services.BuildServiceProvider());
+        }
+
+        public void Register(Type service, Type implementation)
+        {
+            services.AddSingleton(service, implementation);
+        }
+
+        public void RegisterInstance(Type service, object implementation)
+        {
+            services.AddSingleton(service, implementation);
+        }
+
+        public void RegisterLazy(Type service, Func<object> func)
+        {
+            if (func is null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+            services.AddSingleton(service, (provider) => func());
+        }
+    }
+
+    public sealed class TypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly IServiceProvider _provider;
+
+        public TypeResolver(IServiceProvider provider)
+        {
+            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        }
+
+        public object? Resolve(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            return _provider.GetService(type);
+        }
+
+        public void Dispose()
+        {
+            if (_provider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
