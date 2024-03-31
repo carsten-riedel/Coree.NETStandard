f130f5a21a709def656f3441c0827261a6524880
Updated form Workflow <carstenriedel@outlook.com>
2024-03-31 11:21:17 +0200

    update GetHostedServiceCollection

--- src/Projects/Coree.NETStandard/HostedServicesCollection/ServiceProviderExtensions.cs
+++ src/Projects/Coree.NETStandard/HostedServicesCollection/ServiceProviderExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,7 +16,49 @@ namespace Coree.NETStandard.HostedServicesCollection
     /// </summary>
     public static partial class ServiceProviderExtensions
     {
+        public static IEnumerable<T> GetHostedServiceCollection<T>(this IServiceProvider serviceProvider)
+        {
+            var ret = serviceProvider.GetServices<IHostedService>().OfType<T>();
+
+            if (ret != null)
+            {
+                return ret;
+            }
+
+            throw new InvalidOperationException($"No services registered for implementation type {nameof(T)}.");
+        }
+
+  
+        public static IEnumerable<T> GetActiveHostedServiceCollection<T>(this IServiceProvider serviceProvider) where T : class, IHostedService
+        {
+            // Retrieve all IHostedService instances
+            var hostedServices = serviceProvider.GetServices<IHostedService>();
+
+            // Filter out services based on the 'ExecuteTask' property being a non-completed Task
+            var filteredServices = hostedServices
+                .Where(service =>
+                {
+                    var executeTaskProperty = service.GetType().GetProperty("ExecuteTask", BindingFlags.Public | BindingFlags.Instance);
+                    if (executeTaskProperty != null)
+                    {
+                        var executeTaskValue = executeTaskProperty.GetValue(service) as Task;
+                        // Ensure the task is not null and has not completed
+                        return executeTaskValue != null && !executeTaskValue.IsCompleted;
+                    }
+                    return false; // Exclude services without an 'ExecuteTask' property or with a null value
+                })
+                .OfType<T>();
+
+            // Check if any services are found after filtering
+            if (filteredServices.Any())
+            {
+                return filteredServices;
+            }
+            else
+            {
+                return Enumerable.Empty<T>();
+            }
+        }
 
     }
-
 }
