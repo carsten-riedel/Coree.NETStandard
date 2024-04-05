ad7bccd9d1c3286aeefd56a6843e77f055a2639e
Updated form Workflow <carstenriedel@outlook.com>
2024-04-05 17:16:35 +0200

    DependencySingleton  SetMinimumLogLevel

--- src/Projects/Coree.NETStandard/Abstractions/DependencySingleton.cs
+++ src/Projects/Coree.NETStandard/Abstractions/DependencySingleton.cs
@@ -15,39 +15,40 @@ namespace Coree.NETStandard.Abstractions
     /// This pattern can be utilized in both dependency injection (DI) and non-DI scenarios.
     /// Example usage:
     /// <code>
-    /// 
+    ///
     /// //File ISampleSerivce.cs
     /// public interface ISampleService
     /// {
     ///     Task StandardDelayAsync(CancellationToken cancellationToken = default);  void StandardDelay();
     /// }
-    /// 
+    ///
     /// //File DSSampleSerivce.cs
     /// public partial class SampleService : DependencySingleton&lt;SampleService&gt;, ISampleService
     /// {
     ///     public SampleService(ILogger&lt;SampleService&gt; logger, IConfiguration configuration) : base(logger, configuration) { }
     /// }
-    /// 
+    ///
     /// //File SampleSerivce.cs
     /// public partial class SampleService : DependencySingleton&lt;SampleService&gt;, ISampleService
     /// {
     ///     public void StandardDelay() { StandardDelayAsync(CancellationToken.None).GetAwaiter().GetResult(); }
     ///     public async Task StandardDelayAsync(CancellationToken cancellationToken = default)  { await Task.Delay(5000, cancellationToken); }
     /// }
-    /// 
+    ///
     /// //File Program.cs
     /// static async Task Main(string[] args)
     /// {
+    ///    SampleService.Instance.SetMinimumLogLevel(LogLevel.Trace);
     ///    // The ILogger&lt;SampleService&gt; logger and IConfiguration configuration will be initialized with their own service stacks.
     ///    SampleService.Instance.StandardDelay();
-    ///         
+    ///
     ///    // Normal DI usage
     ///    var host = Host.CreateDefaultBuilder(args)
     ///        .ConfigureLogging((context, logging) => {  logging.AddConsole(); logging.AddDebug(); })
     ///        .ConfigureServices((context, services) => { services.AddSingleton&lt;ISampleService,SampleService&gt;(); services.AddSingleton&lt;IMyService,MyService&gt;(); });
     ///    await host.Build().RunAsync();
     /// }
-    /// 
+    ///
     /// //Service usage
     /// public class MyService
     /// {
@@ -72,12 +73,61 @@ namespace Coree.NETStandard.Abstractions
         private static readonly IServiceCollection services = new ServiceCollection();
         private static IServiceProvider? serviceProvider;
 
+        private static LogLevel _minimumLogLevel = LogLevel.Information;
+        private static readonly object logLevelLock = new object();
+
+        /// <summary>
+        /// Private property for getting or setting the minimum log level in a thread-safe manner.
+        /// This log level acts as a global filter across the DependencySingleton, determining the verbosity of the logging output.
+        /// Default is LogLevel.Information
+        /// </summary>
+        private static LogLevel MinimumLogLevel
+        {
+            get
+            {
+                lock (logLevelLock)
+                {
+                    return _minimumLogLevel;
+                }
+            }
+            set
+            {
+                lock (logLevelLock)
+                {
+                    _minimumLogLevel = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the minimum log level for the application. This level acts as a filter for the logs that are emitted.
+        /// Logs below this level will not be emitted.
+        /// </summary>
+        /// <param name="logLevel">The log level to set as the minimum threshold for logging.</param>
+        public void SetMinimumLogLevel(LogLevel logLevel = LogLevel.Trace)
+        {
+            MinimumLogLevel = logLevel;
+        }
+
         /// <summary>
         /// Gets the singleton instance of the class, ensuring thread safety.
         /// </summary>
         public static T Instance => instance.Value;
 
+        /// <summary>
+        /// Provides logging capabilities for the singleton instance. By default, the logger's minimum logging level is set to Trace,
+        /// allowing all log messages to be captured. However, the default filter level applied to log messages is set to Information,
+        /// meaning that only logs at Information level or higher will be emitted unless otherwise adjusted. The logging level filter
+        /// can be dynamically changed at runtime using the <see cref="SetMinimumLogLevel"/> method to control the verbosity of the logging output.
+        /// </summary>
         protected readonly ILogger<T> logger;
+
+        /// <summary>
+        /// Represents the configuration for the application. This encompasses settings from
+        /// various configuration sources (e.g., appsettings.json, environment variables) and
+        /// is used to access DependencySingleton settings such as connection strings, feature
+        /// toggles, and other configurable aspects of the application.
+        /// </summary>
         protected readonly IConfiguration configuration;
 
         /// <summary>
@@ -107,6 +157,7 @@ namespace Coree.NETStandard.Abstractions
                     {
                         builder.AddConsole();
                         builder.AddDebug();
+                        builder.AddFilter((category, level) => level >= MinimumLogLevel);
                     });
 
                     var configuration = new ConfigurationBuilder()
@@ -135,5 +186,4 @@ namespace Coree.NETStandard.Abstractions
             }
         }
     }
-
 }
