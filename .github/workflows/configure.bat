87006a479e6b6b9e7c88a6344695f12747df085b
Updated form Workflow <carstenriedel@outlook.com>
2024-04-09 22:23:36 +0200

    update

--- .github/workflows/configure.bat
+++ .github/workflows/configure.bat
@@ -1,3 +1,9 @@
-REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0"
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 7.0"
-REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 8.0"
+REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0 -Installdir $HOME\cli -Nopath"
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 7.0 -Installdir $HOME\.cli -Nopath"
+REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 8.0 -Installdir $HOME\cli -Nopath"
+
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Environment]::SetEnvironmentVariable('DOTNET_ROOT', \"$HOME\cli\", [System.EnvironmentVariableTarget]::User)"
+powershell -NoProfile -ExecutionPolicy Unrestricted -Command "$path = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User); $newPath = $path + \";$HOME\.cli;$HOME\.cli\tools\""; [System.Environment]::SetEnvironmentVariable('PATH', $newPath, [System.EnvironmentVariableTarget]::User)"
+
+
--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 6.0
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0 -Installdir $HOME/.cli -Nopath
 #curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 8.0
+
+export DOTNET_ROOT=$HOME/.cli
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools'
