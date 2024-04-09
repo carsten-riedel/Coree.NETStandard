3a527ce01194b0b79e14d25ccc41538c60a5604e
Updated form Workflow <carstenriedel@outlook.com>
2024-04-09 23:00:22 +0200

    update

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -4,6 +4,6 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0 -I
 #curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 8.0
 
 
-echo -n '"DOTNET_ROOT=$HOME/.cli"' > DOTNETROOT.txt
+echo -n 'DOTNET_ROOT=$HOME/.cli' > DOTNETROOT.txt
 echo -n '$HOME/.cli/tools:' > DOTNETTOOLSPATH.txt
 
