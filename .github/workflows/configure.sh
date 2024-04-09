db4ee2b3f7d3758fe4baeed315cbde5bd3c528b7
Updated form Workflow <carstenriedel@outlook.com>
2024-04-09 22:24:32 +0200

    fix

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -4,4 +4,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0 -I
 #curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 8.0
 
 export DOTNET_ROOT=$HOME/.cli
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools'
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
