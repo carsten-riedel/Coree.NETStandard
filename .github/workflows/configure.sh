a4a6f1781ae4bb8d92c3284debc9b69ff8b3cd76
Updated form Workflow <carstenriedel@outlook.com>
2024-04-09 21:42:03 +0200

    test

---
+++ .github/workflows/configure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 6.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 8.0
