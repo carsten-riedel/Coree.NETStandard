d2142189805c890b4a8bd9d51f7af5d06e163632
Carsten Riedel <carstenriedel@outlook.com>
2024-04-11 00:43:07 +0200

    update

--- .github/workflows/builddeploy.ps1
+++ .github/workflows/builddeploy.ps1
@@ -101,7 +101,7 @@ Log-Block -Stage "Setup" -Section "Base" -Task "Install powershell modules"
 
 if (-not (Test-CommandAvailability -CommandName "New-PGPKey"))
 {
-    Install-Module -Name PSPGP -AcceptLicense -AllowClobber -AllowPrerelease -Force
+    #Install-Module -Name PSPGP -AcceptLicense -AllowClobber -AllowPrerelease -Force
 }
 
 ######################################################################################
