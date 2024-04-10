3d70642ae0b43df62c1d2e17d558e1a68b39d4a4
Updated form Workflow <carstenriedel@outlook.com>
2024-04-10 21:39:41 +0200

    foo

--- .github/workflows/builddeploy.ps1
+++ .github/workflows/builddeploy.ps1
@@ -1,3 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
 Get-ChildItem Env:* | Select-Object -Property Name,Value
+
+
+$branch = if ($env:GITHUB_HEAD_REF) { $env:GITHUB_HEAD_REF } else { $env:GITHUB_REF -replace 'refs/heads/', '' }
+
+Write-Output "Branchname is $branch"
