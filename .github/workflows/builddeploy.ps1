75d947e28e7a5c3dd334e463905a3369cd781f9e
Updated form Workflow <carstenriedel@outlook.com>
2024-04-11 00:04:12 +0200

    update

--- .github/workflows/builddeploy.ps1
+++ .github/workflows/builddeploy.ps1
@@ -4,10 +4,97 @@ $ErrorActionPreference = 'Stop'
 
 Get-ChildItem Env:* | Select-Object -Property Name,Value
 
+######################################################################################
+Log-Block -Stage "Initialization" -Section "Base" -Task "Resolve branchname and action."
 
-$branch = if ($env:GITHUB_HEAD_REF) { $env:GITHUB_HEAD_REF } else { $env:GITHUB_REF -replace 'refs/heads/', '' }
+$isInGitRepository = IsGit
+if (-not $isInGitRepository) {
+    Write-Error "This script is not running in a git environment."
+    exit 1 # Non-zero exit code indicates an error condition
+}
 
-$gitBranch = git rev-parse --abbrev-ref HEAD
+$initialBranchName = Get-GitBranchName
+if (-not $initialBranchName) {
+    Write-Error "Failed to determine the git branch name."
+    exit 1 # Non-zero exit code indicates an error condition
+}
 
-Write-Output "branch is $branch"
-Write-Output "gitBranch is $gitBranch"
+$isGithubActions = IsGithubActions
+$branchName = Get-GitBranchName
+$branchNameParts = @(Get-BranchNameParts -branchName $branchName)
+$firstBranchSegment = $branchNameParts[0]
+
+if ($isGithubActions -eq $true) { Write-Output "Is github actions." } else { "Is not github actions." }
+Write-Output "Branch name is $branchName"
+Write-Output "BranchRootName name is $firstBranchSegment"
+
+# Define the array of strings
+$isValidBranchRootName = @("feature", "develop", "release", "master" , "hotfix" )
+
+if (-not($isValidBranchRootName.ToLower() -contains $firstBranchSegment.ToLower())) {
+    Write-Error "No configuration for branches $firstBranchSegment"
+    exit 1 # Non-zero exit code indicates an error condition
+}
+
+######################################################################################
+Log-Block -Stage "Initialization" -Section "Base" -Task "Ensure-VariableSet"
+
+$PAT = $args[0]
+$NUGET_PAT = $args[1]
+$NUGET_TEST_PAT = $args[2]
+
+$secretsPath = "$PSScriptRoot/builddeploysecrets.ps1"
+# Check if the secrets file exists before importing
+if (Test-Path $secretsPath) {
+    . "$secretsPath"
+}
+
+Ensure-VariableSet -VariableName "`$PAT" -VariableValue "$PAT"
+Ensure-VariableSet -VariableName "`$NUGET_PAT" -VariableValue "$NUGET_PAT"
+Ensure-VariableSet -VariableName "`$NUGET_TEST_PAT" -VariableValue "$NUGET_TEST_PAT"
+
+######################################################################################
+Log-Block -Stage "Initialization" -Section "Base" -Task "Ensure-CommandAvailability"
+
+Ensure-CommandAvailability -CommandName "dotnet"
+Ensure-CommandAvailability -CommandName "pwsh"
+Ensure-CommandAvailability -CommandName "powershell"
+
+######################################################################################
+Log-Block -Stage "Initialization" -Section "Base" -Task "Config values for branches"
+
+if ($firstBranchSegment -ieq "feature") {
+
+} elseif ($firstBranchSegment -ieq "develop") {
+
+} elseif ($firstBranchSegment -ieq "release") {
+
+} elseif ($firstBranchSegment -ieq "master") {
+
+} elseif ($firstBranchSegment -ieq "hotfix") {
+
+}
+
+######################################################################################
+Log-Block -Stage "Setup" -Section "Base" -Task "Install dotnet tools"
+
+if (-not (Test-CommandAvailability -CommandName "docfx"))
+{
+    dotnet tool install --global docfx --version 2.74.1
+}
+
+######################################################################################
+Log-Block -Stage "Setup" -Section "Base" -Task "Install powershell modules"
+
+if (-not (Test-CommandAvailability -CommandName "New-PGPKey"))
+{
+    Install-Module -Name PSPGP
+}
+
+######################################################################################
+Log-Block -Stage "Build" -Section "Prepare" -Task ""
+
+Clear-BinObjDirectories -sourceDirectory "src/Projects/Coree.NETStandard"
+
+
+### continue
--- .github/workflows/builddeploy_helper.ps1
+++ .github/workflows/builddeploy_helper.ps1
@@ -1,3 +1,126 @@
+function Get-GithubActionsBranchName {
+    if ($env:GITHUB_HEAD_REF) {
+        return $env:GITHUB_HEAD_REF
+    } elseif ($env:GITHUB_REF) {
+        return $env:GITHUB_REF -replace 'refs/heads/', ''
+    }
+    return $null
+}
 
+function Get-GitBranchName {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+        return $branch
+    } catch {
+        return $null
+    }
+}
 
-Write-Output "dotsourced"
+function IsGithubActions {
+    $githubActionsBranchName = Get-GithubActionsBranchName
+    return -not [string]::IsNullOrWhiteSpace($githubActionsBranchName)
+}
+
+function IsGit {
+    # Attempt to get the GitHub Actions branch name
+    $githubBranchName = Get-GithubActionsBranchName
+    # Attempt to get the local git branch name
+    $gitBranchName = Get-GitBranchName
+
+    # Check if either GitHub Actions branch name or local git branch name is available
+    if (-not [string]::IsNullOrWhiteSpace($githubBranchName) -or -not [string]::IsNullOrWhiteSpace($gitBranchName)) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+function Get-BranchNameParts {
+    param (
+        [string]$branchName
+    )
+
+    # Split the branch name by the '/' delimiter
+    $parts = $branchName -split '/'
+
+    # Return the array of parts
+    return $parts
+}
+
+function Log-Block {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
+    param (
+        [string]$Stage,
+        [string]$Section,
+        [string]$Task
+    )
+    Write-Output "_"
+    Write-Output "==============================================================================================================="
+    if (-not [string]::IsNullOrEmpty($Stage)) {
+        $output =  "Stage: {0} Section: {1} Task: {2} " -f $Stage.PadRight(15), $Section.PadRight(20), $Task.PadRight(35)
+        Write-Output $output
+    }
+    Write-Output "==============================================================================================================="
+}
+
+function Test-CommandAvailability {
+    param (
+        [string]$CommandName
+    )
+
+    try {
+        $null = Get-Command $CommandName -ErrorAction Stop
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+function Ensure-CommandAvailability {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$CommandName
+    )
+    process {
+        $isAvailible = $false
+
+        try {
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments','')]
+            $commandInfo = Get-Command $CommandName -ErrorAction Stop
+            $isAvailible = $true
+            $output = "Command is available   : {0}" -f $CommandName
+            Write-Output $output
+        }
+        catch {}        
+        if ($isAvailible -eq $false)
+        {
+            $output = "Command is not available : {0}" -f $CommandName
+            throw "$output";
+        }
+    }
+}
+
+function Ensure-VariableSet {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$VariableName,
+        
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [AllowNull()]
+        [string]$VariableValue
+    )
+    process {
+        if ([string]::IsNullOrEmpty($VariableValue)) {
+            $output = "VariableName: {0} is not set." -f $VariableName.PadRight(30)
+            Write-Output $output
+            throw "$output";
+        }
+        else {
+            Write-Output ("VariableName: {0} is set." -f $VariableName.PadRight(30))
+        }
+    }
+}
+
