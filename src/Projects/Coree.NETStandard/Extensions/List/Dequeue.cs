3c1c7e80c32e1823b6fe3ce7832fa2016c91e0f9
Carsten Riedel <carstenriedel@outlook.com>
2024-04-15 18:41:54 +0200

    update from Coree.Extension

---
+++ src/Projects/Coree.NETStandard/Extensions/List/Dequeue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coree.NETStandard.Extensions
+{
+
+    /// <summary>
+    /// Contains extension methods for enhancing the functionality of the List class and other list-like collections.
+    /// </summary>
+    public static partial class ListExtension
+    {
+        /// <summary>
+        /// Removes and returns the object at the beginning of the System.Collections.Generic.List`1.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns>The object that is removed from the beginning of the System.Collections.List.List`1.</returns>
+        /// <exception cref="InvalidOperationException">The System.Collections.Generic.List`1 is empty</exception>
+        /// <exception cref="NullReferenceException"></exception>
+        public static T? Dequeue<T>(this List<T> values) where T : class
+        {
+            T? retval = null;
+            if (values != null && values.Count >= 1)
+            {
+                retval = values[0];
+                values.RemoveAt(0);
+            }
+            else if (values != null && values.Count == 0)
+            {
+                throw new InvalidOperationException("Queue empty.");
+
+            }
+            else if (values == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return retval;
+        }
+    }
+}
---
+++ src/Projects/Coree.NETStandard/Extensions/List/Enqueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coree.NETStandard.Extensions
+{
+
+    /// <summary>
+    /// Contains extension methods for enhancing the functionality of the List class and other list-like collections.
+    /// </summary>
+    public static partial class ListExtension
+    {
+        /// <summary>
+        /// Adds an object to the end of the System.Collections.Generic.List`1.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <param name="item">The object to add to the System.Collections.Generic.List`1. The value can be null for reference types</param>
+        /// <exception cref="NullReferenceException"></exception>
+        public static void Enqueue<T>(this List<T> values, T item) where T : class
+        {
+            if (values != null)
+            {
+                values.Add(item);
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
