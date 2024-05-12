149e3402687c10fd30b54960a8c0c022f8dfd631
Carsten Riedel <carstenriedel@outlook.com>
2024-05-12 09:29:27 +0200

    update post

--- .github/workflows/cicd.ps1
+++ .github/workflows/cicd.ps1
@@ -201,41 +201,10 @@ Execute-Command "git push origin ""$tag"""
 git config user.name $gitUserLocal
 git config user.email $gitMailLocal
 
-######################################################################################
-Log-Block -Stage "Post Deploy" -Section "Cleanup Packagelist" -Task ""
 
-$headers = @{
-    Authorization = "Bearer $PAT"
-}
+. "$PSScriptRoot/cicd_post_clean.ps1"
 
-$GitHubNugetPackagelist = Invoke-RestMethod -Uri "https://api.github.com/users/$gitOwner/packages/nuget/$gitRepo/versions" -Headers $headers | Out-Null
-$GitHubNugetPackagelistOld = $GitHubNugetPackagelist | Where-Object { $_.name -like "*$branchNameSegment" } | Sort-Object -Property created_at -Descending | Select-Object -Skip 2
-foreach ($item in $GitHubNugetPackagelistOld)
-{
-    $PackageId = $item.id
-    Invoke-RestMethod -Method Delete -Uri "https://api.github.com/users/$gitOwner/packages/nuget/$gitRepo/versions/$PackageId" -Headers $headers | Out-Null
-    Write-Output "Unlisted package $gitRepo $($item.name)"
-}
-
-######################################################################################
-Log-Block -Stage "Call" -Section "Dispatch" -Task "dispatching a other job"
-
-if ($branchNameSegment -ieq "master") {
-    $worklowFileName = "static.yml"
-    $uri = "https://api.github.com/repos/$gitOwner/$gitRepo/actions/workflows/$worklowFileName/dispatches"
-    $headers = @{
-        "Accept" = "application/vnd.github+json"
-        "X-GitHub-Api-Version" = "2022-11-28"
-        "Authorization" = "Bearer $PAT"
-        "Content-Type" = "application/json"
-    }
-    $body = @{
-        ref = "$branchName"
-    } | ConvertTo-Json
-    
-    Invoke-WebRequest -Uri $uri -Method Post -Headers $headers -Body $body -Verbose | Out-Null
-}
-
+. "$PSScriptRoot/cicd_post_run.ps1"
 
 #git status --porcelain $sourceCodeFolder
 
