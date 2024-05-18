56f3a207e68deb6d9628deb99a63ad18edf77cef
Carsten Riedel <carstenriedel@outlook.com>
2024-05-18 09:15:16 +0200

    update

--- .github/workflows/cicd_main.ps1
+++ .github/workflows/cicd_main.ps1
@@ -18,7 +18,7 @@ $NUGET_TEST_PAT = $args[2]
 . "$PSScriptRoot/cicd_build_clean.ps1"
 . "$PSScriptRoot/cicd_build_config.ps1"
 . "$PSScriptRoot/cicd_build.ps1"
-#. "$PSScriptRoot/cicd_deploy.ps1"
+. "$PSScriptRoot/cicd_deploy.ps1"
 #. "$PSScriptRoot/cicd_postdeploy_clean.ps1"
 #. "$PSScriptRoot/cicd_postdeploy_run.ps1"
 
