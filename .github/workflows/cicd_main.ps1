f736eb3f66fd659ed8ece05d86e035d7816389fa
Carsten Riedel <carstenriedel@outlook.com>
2024-05-18 07:34:26 +0200

    update

--- .github/workflows/cicd_main.ps1
+++ .github/workflows/cicd_main.ps1
@@ -14,9 +14,9 @@ Start-Transcript -Path "$Transcript"
 . "$PSScriptRoot/cicd_build_clean.ps1"
 . "$PSScriptRoot/cicd_build_config.ps1"
 . "$PSScriptRoot/cicd_build.ps1"
-. "$PSScriptRoot/cicd_deploy.ps1"
-. "$PSScriptRoot/cicd_postdeploy_clean.ps1"
-. "$PSScriptRoot/cicd_postdeploy_run.ps1"
+#. "$PSScriptRoot/cicd_deploy.ps1"
+#. "$PSScriptRoot/cicd_postdeploy_clean.ps1"
+#. "$PSScriptRoot/cicd_postdeploy_run.ps1"
 
 #git status --porcelain $sourceCodeFolder
 
