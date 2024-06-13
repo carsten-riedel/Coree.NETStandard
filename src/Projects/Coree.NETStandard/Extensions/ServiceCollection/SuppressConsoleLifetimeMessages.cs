c843517e5aad16b4ca9d0cddb760c376b7ba14cd
Carsten Riedel <carstenriedel@outlook.com>
2024-06-13 22:00:02 +0200

    SuppressConsoleLifetimeMessages + ForSourceContext

---
+++ src/Projects/Coree.NETStandard/Extensions/ServiceCollection/SuppressConsoleLifetimeMessages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Coree.NETStandard.Extensions.ServiceCollection
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="IServiceCollection"/> to configure application services.
+    /// </summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configures ConsoleLifetimeOptions to suppress status messages.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to configure.</param>
+        public static IServiceCollection SuppressConsoleLifetimeMessages(this IServiceCollection services)
+        {
+            services.Configure<ConsoleLifetimeOptions>(options =>
+            {
+                options.SuppressStatusMessages = true;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Projects/Coree.NETStandard/Serilog/ForSourceContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+using SerilogCore = Serilog;
+
+namespace Coree.NETStandard.Serilog
+{
+    /// <summary>
+    /// Provides extension methods for Serilog ILogger instances to enhance logging capabilities.
+    /// </summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configures the logger to associate a specified source context with log events.
+        /// </summary>
+        /// <param name="logger">The Serilog ILogger instance to configure.</param>
+        /// <param name="value">The value to set for the SourceContext property in the log context.</param>
+        /// <returns>A new ILogger instance that includes the specified source context.</returns>
+        /// <example>
+        /// Here is how you can use the <c>ForSourceContext</c> method:
+        /// <code>
+        /// var logger = Log.Logger;
+        /// var contextualLogger = logger.ForSourceContext("MyApplication.Component");
+        /// contextualLogger.Information("This is an informational message with a source context.");
+        /// </code>
+        /// </example>
+        public static SerilogCore.ILogger ForSourceContext(this SerilogCore.ILogger logger, string value)
+        {
+            return logger.ForContext("SourceContext", value);
+        }
+    }
+}
